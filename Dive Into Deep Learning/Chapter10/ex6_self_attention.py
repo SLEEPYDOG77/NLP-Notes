e30dc56c9ad7655b14b44e2cc84f671db3878ac7
SLEEPY_DOG <709918448@qq.com>
2022-03-02 21:20:13 +0800

    [update] Dive Into Deep Learning

--- Dive Into Deep Learning/Chapter10/ex6_self_attention.py
+++ Dive Into Deep Learning/Chapter10/ex6_self_attention.py
@@ -8,7 +8,7 @@ import math
 import torch
 from torch import nn
 from d2l import torch as d2l
-from d2l import MultiHeadAttention
+from d2l import MultiHeadAttention, plot, plt
 
 # 自注意力
 num_hiddens, num_heads = 100, 5
@@ -29,3 +29,12 @@ class PositionalEncoding(nn.Module):
     def forward(self, X):
         X = X + self.P[:, :X.shape[1], :].to(X.device)
         return self.dropout(X)
+
+encoding_dim, num_steps = 32, 60
+pos_encoding = PositionalEncoding(encoding_dim, 0)
+pos_encoding.eval()
+X = pos_encoding(torch.zeros((1, num_steps, encoding_dim)))
+P = pos_encoding.P[:, :X.shape[1], :]
+plt.plot(torch.arange(num_steps), P[0, :, 6:10].T, xlabel='Row (position)',
+         figsize=(6, 2.5), legend=["Col %d" % d for d in torch.arange(6, 10)])
+plt.show()
